6d3fd0444a333cd1528f24794c1c0480eadaa14d
Vahagn <khorotyanvahagn@gmail.com>
2024-03-23 22:26:32 +0100

    Show platform

--- counter-frontend/src/App.tsx
+++ counter-frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { fromNano } from "ton-core";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const {
@@ -22,6 +23,7 @@ function App() {
       </div>
       <div>
         <div className="Card">
+          <b>{WebApp.platform} </b>
           <b>Our contract Address</b>
           <div className="Hint">{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
