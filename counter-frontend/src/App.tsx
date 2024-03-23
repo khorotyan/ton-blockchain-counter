a768f6cc84abfeb36184a9e9524cccba928efa24
Vahagn <khorotyanvahagn@gmail.com>
2024-03-23 05:20:30 +0100

    Deploy script

--- counter-frontend/src/App.tsx
+++ counter-frontend/src/App.tsx
@@ -8,14 +8,11 @@ function App() {
   const {
     contract_address,
     counter_value,
-    recent_sender,
-    owner_address,
     contract_balance,
     sendIncrement,
     sendDeposit,
     sendWithdrawalRequest,
   } = useMainContract();
-
   const { connected } = useTonConnect();
 
   return (
